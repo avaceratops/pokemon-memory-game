488fe40fdb85dd2c983ead49699326db557f6cdc
ava <148492613+avaceratops@users.noreply.github.com>
2024-01-30 14:40:05 +0000

    Add Card component
    Returns a clickable card comprised of a Pokemon's sprite and name.

---
+++ src/components/Card.jsx
@@ -0,0 +1,10 @@
+import '../styles/Card.scss';
+
+export default function Card({ id, name, sprite, onClick }) {
+  return (
+    <button className="pokemon-card" data-id={id} onClick={onClick}>
+      <img className="pokemon-card__sprite" src={sprite} alt="" />
+      <h2 className="pokemon-card__name">{name}</h2>
+    </button>
+  );
+}
