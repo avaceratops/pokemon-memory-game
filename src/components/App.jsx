9b17a49adc42e351376f4375996b812c6a3c364f
ava <148492613+avaceratops@users.noreply.github.com>
2024-01-31 11:50:29 +0000

    Update App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,9 +1,15 @@
+import Game from './Game';
 import '../styles/App.scss';
 
 function App() {
   return (
     <>
-      <h1>Pokémon Memory Game</h1>
+      <h1 className="title">Pokémon Memory Game</h1>
+      <p className="desc">
+        Click on each Pokémon to gain points, being careful not to click the same one twice. Catch
+        &apos;em all without repeating!
+      </p>
+      <Game></Game>
     </>
   );
 }
