3a681aece53df8eaf7f1adce172090b874282062
ava <148492613+avaceratops@users.noreply.github.com>
2024-01-31 11:59:40 +0000

    Add landmark element to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,14 +3,14 @@ import '../styles/App.scss';
 
 function App() {
   return (
-    <>
+    <main>
       <h1 className="title">Pokémon Memory Game</h1>
       <p className="desc">
         Click on each Pokémon to gain points, being careful not to click the same one twice. Catch
         &apos;em all without repeating!
       </p>
       <Game></Game>
-    </>
+    </main>
   );
 }
 
